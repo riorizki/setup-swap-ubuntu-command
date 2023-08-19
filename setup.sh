b37929aa2afe6cf3c9c275aadd0d4859b94c4c73
Rio Rizki <rio.rizkiw21@gmail.com>
2023-08-19 14:29:40 +0700

    first commit

---
+++ setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -e
+
+function getCurrentDir() {
+    local current_dir="${BASH_SOURCE%/*}"
+    if [[ ! -d "${current_dir}" ]]; then current_dir="$PWD"; fi
+    echo "${current_dir}"
+}
+
+function includeDependencies() {
+    # shellcheck source=./setupLibrary.sh
+    source "${current_dir}/setupLibrary.sh"
+}
+
+current_dir=$(getCurrentDir)
+includeDependencies
+output_file="output.log"
+
+function main() {
+    echo 'Kajaaa...'
+
+    if ! hasSwap; then
+        setupSwap
+    fi
+
+    setupTimezone
+
+    echo "Configuring System Time... " >&3
+    configureNTP
+
+    # sudo service ssh restart
+
+    cleanup
+
+    echo "Setup Done! Log file is located at ${output_file}" >&3
+}
+
+function setupSwap() {
+    createSwap
+    mountSwap
+    tweakSwapSettings "10" "50"
+    saveSwapSettings "10" "50"
+}
+
+function hasSwap() {
+    [[ "$(sudo swapon -s)" == *"/swapfile"* ]]
+}
+
+function cleanup() {
+    if [[ -f "/etc/sudoers.bak" ]]; then
+        revertSudoers
+    fi
+}
+
+function logTimestamp() {
+    local filename=${1}
+    {
+        echo "===================" 
+        echo "Log generated on $(date)"
+        echo "==================="
+    } >>"${filename}" 2>&1
+}
+
+function setupTimezone() {
+    echo -ne "Enter the timezone for the server (Default is 'Asia/Singapore'):\n" >&3
+    read -r timezone
+    if [ -z "${timezone}" ]; then
+        timezone="Asia/Singapore"
+    fi
+    setTimezone "${timezone}"
+    echo "Timezone is set to $(cat /etc/timezone)" >&3
+}
+
+main
